2ade68b020ad237bcfdff4ef4f550d2c0bdfd233
Kory Dondzila <korydondzila@gmail.com>
2017-11-27 16:50:52 -0800

    Starting to make gameboard on main app.

--- App/src/app/app.component.ts
+++ App/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
+  selector: 'board',
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.css']
+})
+export class BoardComponent {
+}
+
+@Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Connect Four';
 }
--- App/src/app/app.module.ts
+++ App/src/app/app.module.ts
@@ -2,12 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
-import { AppComponent } from './app.component';
+import { AppComponent, BoardComponent } from './app.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardComponent
   ],
   imports: [
     BrowserModule
